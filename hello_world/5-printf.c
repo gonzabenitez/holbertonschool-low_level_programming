ecf27828725edcbf562276d8b0db550b8e7948b0
Gonza <gonzabenitez070@gmail.com>
2022-06-06 10:17:13 -0700

    Add printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succeess)
+*/
+
+int main(void)
+{
+
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
