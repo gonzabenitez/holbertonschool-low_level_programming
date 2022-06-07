1bda28a450ab6f76c5d9244994f65db5d7f878fe
Gonza <gonzabenitez070@gmail.com>
2022-06-07 13:02:17 -0700

    Add print comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succeess)
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = '0' ; a <= '9' ; a++)
+	{
+	putchar(a);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
