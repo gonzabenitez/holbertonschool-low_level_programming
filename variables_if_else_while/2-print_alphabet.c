7a80094fe84912583d722408568c611bc79037de
Gonza <gonzabenitez070@gmail.com>
2022-06-07 09:52:32 -0700

    Add print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succeess)
+*/
+
+int main(void)
+{
+	for (char a='a' ; a<='z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
