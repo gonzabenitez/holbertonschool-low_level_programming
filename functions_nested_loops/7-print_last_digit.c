bed420d0d51958b9b1561d9f8b50a15a8236f008
Gonza <gonzabenitez070@gmail.com>
2022-06-09 08:23:27 -0700

    Add pld

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * print_last_digit - guess it
+ *@i: Character value
+ *
+ * Return:  0 if Succeess
+*/
+
+int print_last_digit(int i)
+{
+	i = i % 10;
+	_putchar(i - '0');
+	return (i);
+}
