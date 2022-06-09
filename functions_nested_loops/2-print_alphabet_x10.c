3277cf362c341b03bd8f4d553deee94c5e2ee945
Gonza <gonzabenitez070@gmail.com>
2022-06-09 06:53:40 -0700

    add print alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * print_alphabet_x10 - guess it
+ *
+ * Return: Always 0 (Succeess)
+*/
+
+void print_alphabet_x10(void)
+{
+	char c, d;
+
+	for (c = 1 ; c <= 10 ; c++)
+	{
+		for (d = 'a' ; d <= 'z' ; d++)
+			_putchar(d);
+	}
+	_putchar('\n');
+
+}
