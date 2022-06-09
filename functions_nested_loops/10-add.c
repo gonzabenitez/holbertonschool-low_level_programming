e16bbbcb27848b0c4eef0ba4580b6a129afdda24
Gonza <gonzabenitez070@gmail.com>
2022-06-09 10:29:16 -0700

    Add add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * add - guess it
+ *@i: Character value
+ *@j: Other value
+ *
+ * Return:  0 if Succeess
+*/
+
+int add(int i, int j)
+{
+	i += j;
+	return (i);
+}
