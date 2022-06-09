93cb5ec9be7abed1ac836f0cd1f78a3e4bb31033
Gonza <gonzabenitez070@gmail.com>
2022-06-09 10:49:14 -0700

    Add print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * print_to_98 - guess it
+ *@i: whre to count from
+ *
+ * Return: Always 0 (Succeess)
+*/
+
+void print_to_98(int i)
+{
+	int j, k;
+
+	if (i > 98)
+		j = -1;
+	else
+		j = 1;
+
+	while ( i != 98 )
+	{
+		printf("%d, ", i);
+		i += j;
+	}
+	printf("%d\n",98);
+
+}
