52b4a9940b9eb84bcc8824df665c3b0a4621440d
Gonza <gonzabenitez070@gmail.com>
2022-06-09 08:08:38 -0700

    Add abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * _abs - guess it
+ *@i: Character value
+ *
+ * Return:  0 if Succeess
+*/
+
+int _abs(int i)
+{
+	if (i > 0 )
+	{
+		return (i);
+	}
+	else if (i < 0)
+	{
+		
+		return ((-1*i));
+	}
+	else
+	{
+		return (0);
+	}
+}
