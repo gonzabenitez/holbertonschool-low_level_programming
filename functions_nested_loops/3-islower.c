8b0dd080128df3780ed4bf850bcc75929df717cc
Gonza <gonzabenitez070@gmail.com>
2022-06-09 07:07:59 -0700

    Add islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * print_alphabet_x10 - guess it
+ *
+ * Return: Always 0 (Succeess)
+*/
+
+int _islower(int i)
+{
+	if (i >='a' && i <= 'z')
+		return (1);
+	else 
+		return (0);
+
+}
