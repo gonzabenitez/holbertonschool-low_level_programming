e3a726a155fae54a5e08ecafc617f942bfb1be47
Gonza <gonzabenitez070@gmail.com>
2022-06-10 06:33:12 -0700

    Add more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * print_most_numbers- guess it
+ *
+ * Return:  0 if Succeess
+*/
+
+void print_most_numbers(void)
+{
+	char i, j;
+
+	for (i = 0 ; i <= 14 ; i++)
+	{
+		j = i + '0';
+		if (i > 9)
+			j = (i % 10) + '0';
+			_putchar('1');
+		putchar(j);
+	}
+	_putchar('\n');
+}
