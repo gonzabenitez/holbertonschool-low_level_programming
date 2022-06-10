ef575c7dcd258052cef0fc225026a63021190cb1
Gonza <gonzabenitez070@gmail.com>
2022-06-10 06:02:56 -0700

    Fix function name

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 # include <stdio.h>
 # include "main.h"
 /**
- * _isupper- guess it
+ * _isdigit- guess it
  *@i: Character value
  *
  * Return:  0 if Succeess
 */
 
-int _isupper(int i)
+int _isdigit(int i)
 {
 	if (i >= '0' && i <= '9')
 		return (1);
