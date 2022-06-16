a71f244d8effae162d0b9873781af145ffa56665
Gonza <gonzabenitez070@gmail.com>
2022-06-16 08:53:34 -0700

    Add print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+# include "main.h"
+
+/**
+ * print_rev - Something useful :)
+ *@s: string to be measured
+ * Return: Always 0 (Succeess)
+*/
+
+void print_rev(char *s)
+{
+	int j;
+	int len = _strlen(s);
+	char r[len];
+
+	j = len;
+
+	while (j >= 0)
+	{
+		r[(len - j)] = s[j];
+		j--;
+	}
+}
+/**
+ * _strlen - returns len of a string
+ * @s: string to be measured
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
