70ea3bbd6860e06927a8d2460fcf3f354aeb8c66
Gonza <gonzabenitez070@gmail.com>
2022-06-16 06:37:45 -0700

    Fix typo in folder name >.<'

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+/**
+ * reset_to_98 - Something useful :)
+ *@p: pointer to an int
+ * Return: Always 0 (Succeess)
+*/
+
+void reset_to_98(int *p)
+{
+	*p = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+This folder will hold the tasks for the project
+
+Hope you find it usefull.
+
+Enjoy :)
