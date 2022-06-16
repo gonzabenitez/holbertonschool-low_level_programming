a70db840be531b3774444b6f8326a5607d2f7e68
Gonza <gonzabenitez070@gmail.com>
2022-06-16 07:26:34 -0700

    Add puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+/**
+ * _puts - Something useful :)
+ *@s: string to be measured
+ * Return: Always 0 (Succeess)
+*/
+
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
