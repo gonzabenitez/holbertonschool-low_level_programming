808e511a2bd6fd74c0934cdb6aa127c6ad3971e0
Gonza <gonzabenitez070@gmail.com>
2022-06-18 09:10:09 -0700

    Fix case where i=0

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,14 @@ char *cap_string(char *dest)
 {
 	int i, j, k;
 	int lenD = _strlen(dest);
-	char sep[] = {9, 10, 32,',',';','.','!','?','"','(',')','{','}'};
+	char sep[] = {9, 10, 32, ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; i < lenD; i++)
 	{
 		j = i + 1;
 		for (k = 12; k >= 0; k--)
 		{
-			if (dest[i] == sep[k] && dest[j] >= 'a' && dest [j] <= 'z')
+			if ((dest[i] == sep[k] || i == 0) && dest[j] >= 'a' && dest [j] <= 'z')
 				dest[j] -= 32;
 		}
 	}
