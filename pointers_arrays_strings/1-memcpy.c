9800f1ce6e10c46ff26f3e2c4fce6dfa22bafeb4
Gonza <gonzabenitez070@gmail.com>
2022-06-18 11:41:02 -0700

    Add memecpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,45 @@
+# include "main.h"
+
+/**
+ * _memcpy - Something useful :)
+ *@dest: string to be measured
+ *@src: source string
+ *@n: amount of bytes
+ * Return: Always 0 (Succeess)
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	int lenS = _strlen(src);
+	int lenD = _strlen(dest);
+
+	if (lenS == 0 && lenD == 0)
+		n = 0;
+	else
+		lenS++;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < lenS)
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - returns len of a string
+ * @s: string to be measured
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
