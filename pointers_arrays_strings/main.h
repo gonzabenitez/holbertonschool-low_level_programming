7a0cbfe628c1e3967135fa6cdece5684eb850caa
Gonza <gonzabenitez070@gmail.com>
2022-06-18 11:47:25 -0700

    Typo in header file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,7 +19,7 @@ void reverse_array(int *,int);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-unsigned int _strnlenA(char *);
+unsigned int _strlenA(char *);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 # endif
