e564a915f78e3d540729ba0a227a2d14895cbd44
Gonza <gonzabenitez070@gmail.com>
2022-06-21 11:57:45 -0700

    Add main h

--- recursion/main.h
+++ recursion/main.h
@@ -2,6 +2,7 @@
 # define MAIN_H
 int _putchar(char);
 int _strlen(char *, int);
+void _vstrlen(char *, int);
 void _put_n(char *, int);
 void _puts_recursion(char *);
 void _print_rev_recursion(char *);
