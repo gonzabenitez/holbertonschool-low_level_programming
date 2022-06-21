010ea4d29a9fb4df8f42b1cf70dd6f7e345b1099
Gonza <gonzabenitez070@gmail.com>
2022-06-21 15:35:41 -0700

    Add args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+/**
+ * main - Something useful :)
+ *@argc: string to be measured
+ *@argv: verctor
+ * Return: Always 0 (Succeess)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
