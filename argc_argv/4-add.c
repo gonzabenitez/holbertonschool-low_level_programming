da0b2150ae6d2cead36de0308be40172a09c6bb8
Gonza <gonzabenitez070@gmail.com>
2022-06-21 16:25:21 -0700

    Add add dad daa ddd

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+# include <stdio.h>
+# include <stdlib.h>
+/**
+ * main - Something useful :)
+ *@argc: string to be measured
+ *@argv: verctor
+ * Return: Always 0 (Succeess)
+*/
+
+int main(int argc, char *argv[])
+{
+	long a = 0;
+	int i, errno;
+
+	if (argc < 2)
+	{
+		printf("0");
+		return (0);
+	}
+	for (i = 1; i <= argc; i++)
+	{
+		errno = 0;
+		a += strtol(argv[1], NULL, 10);
+		if (errno != 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+		printf("%ld\n", a);
+
+		return (0);
+	}
+}
