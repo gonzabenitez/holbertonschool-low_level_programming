37d6f56885834ef07e263e5468349188a59adc89
Gonza <gonzabenitez070@gmail.com>
2022-06-21 15:29:07 -0700

    Add everything

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * main - Something useful :)
+ *@argc: string to be measured
+ *@argv: verctor
+ * Return: Always 0 (Succeess)
+*/
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n",argv[0]);
+	return (0);
+}
