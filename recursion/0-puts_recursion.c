53a48ca5401adb173b99d5e353c5ca721587c757
Gonza <gonzabenitez070@gmail.com>
2022-06-21 10:02:24 -0700

    Add folder readme and puts recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,39 @@
+# include "main.h"
+
+/**
+ * _puts_recursion - Something useful :)
+ *@s: string to be measured
+ * Return: Always 0 (Succeess)
+*/
+
+void _puts_recursion(char *s)
+{
+
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+		return;
+	}
+}
+
+/**
+ * _strlen - returns len of a string
+ * @s: string to be measured
+ * Return: length of string
+*/
+
+int _strlen(char *s, int n)
+{
+	if (s[n] != '\0')
+	{
+		n++;
+		_strlen(s, n);
+	}
+	else
+		return (n);
+}
