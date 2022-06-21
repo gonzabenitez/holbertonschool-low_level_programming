006b65cdc0c3aeebc0f2d310101cfbedaf1dfefe
Gonza <gonzabenitez070@gmail.com>
2022-06-21 15:50:11 -0700

    add mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+
+/**
+ * main - Something useful :)
+ *@argc: string to be measured
+ *@argv: verctor
+ * Return: Always 0 (Succeess)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, a, b;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else	
+	{
+		a = argv[1] - 48;
+		b = argv[2] - 48;
+		printf("%d\n", a * b)	
+		return (0);
+	}
+}
