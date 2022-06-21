12df64395f44f8023649786d844abfa4218c9046
Gonza <gonzabenitez070@gmail.com>
2022-06-21 15:59:16 -0700

    add mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 	}
 	else	
 	{
-		a = strtol(argv[1]);
-		b = strtol(argv[2]);
-		printf("%d\n", a * b);	
+		a = strtol(argv[1], NULL, 10);
+		b = strtol(argv[2], NULL, 10);
+		printf("%ld\n", a * b);	
 		return (0);
 	}
 }
