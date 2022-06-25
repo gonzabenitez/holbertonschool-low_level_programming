5eeca581a21780f44943d9ddfca3d69371c3bde1
Gonza <gonzabenitez070@gmail.com>
2022-06-25 13:38:07 -0700

    Add free grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+# include "main.h"
+# include <stdlib.h>
+/**
+ * free_grid - Something useful :)
+ *@grid: string to be measured
+ *@h: source string
+ * Return: Always 0 (Succeess)
+*/
+
+void free_grid(int **grid, int h)
+{
+	int i;
+
+	if (grid != NULL && h > 0)
+	{
+		for (i = 0; i < h; i++)
+			free(dest[i]);
+		free(dest);
+	}
+
+}
+
