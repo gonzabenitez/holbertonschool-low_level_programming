3993a79429855a78441de9b155d32a10cf845f13
Gonza <gonzabenitez070@gmail.com>
2022-06-25 08:57:11 -0700

    First commit

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+# include "main.h"
+
+/**
+ * create_array - Something useful :)
+ *@s: string to be measured
+ *@c: source string
+ * Return: Always 0 (Succeess)
+*/
+
+char *create_array(unsigned int s, char c)
+{
+	unsigned int i;
+	char *dest;
+	if (s == 0)
+		dest = NULL;
+	else
+		dest = malloc(s);
+
+	if (dest != NULL)
+	{
+		for (i = 0; i < s; i++)
+			dest[i] = c;
+	}
+	return (dest);
+}
+
