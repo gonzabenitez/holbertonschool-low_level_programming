a3b5727b62d9c0dfba6125a788ff4a15caba9c4b
Gonza <gonzabenitez070@gmail.com>
2022-06-25 14:02:44 -0700

    Update main

---
+++ more_malloc_free/main.h
@@ -0,0 +1,11 @@
+# ifndef MAIN_H
+# define MAIN_H
+int _putchar(char);
+int _strlen(char *);
+char *create_array(unsigned int, char);
+char *_strdup(char *str);
+char *str_concat(char *,char *);
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+void *malloc_checked(unsigned int);
+# endif
