b22f591ca542e252b7f34dde6bbacc1eb43f3f22
Gonza <gonzabenitez070@gmail.com>
2022-06-25 09:27:12 -0700

    Add strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+# include "main.h"
+
+/**
+ * _strdup - Something useful :)
+ *@str: string to be measured
+ * Return: Always 0 (Succeess)
+*/
+
+char *_strdup(char *str)
+{
+	int i, s;
+	char *dest;
+
+	s = _strlen(str);
+
+	if (s == 0)
+		dest = NULL;
+	else
+		dest = malloc(s);
+
+	if (dest != NULL)
+	{
+		for (i = 0; i < s; i++)
+			dest[i] = str[i];
+	}
+	return (dest);
+
+}
+
+/**
+ * _strlen - returns len of a string
+ * @s: string to be measured
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
