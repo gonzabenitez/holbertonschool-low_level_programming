1c17316be057345646494689cb05438f3c44aa39
Gonza <gonzabenitez070@gmail.com>
2022-06-25 14:38:15 -0700

    Add str concat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,7 @@
  * Return: Always 0 (Succeess)
 */
 
-char *string_nconcat(char *s1, char *s2, int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, s, t;
 	char *dest;
