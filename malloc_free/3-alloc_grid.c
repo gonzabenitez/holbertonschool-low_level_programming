c088f5eff461484b21dc3eb86369da1c68852a60
Gonza <gonzabenitez070@gmail.com>
2022-06-25 12:19:05 -0700

    Add alloc grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+# include "main.h"
+# include <stdlib.h>
+
+/**
+ * alloc_grid - Something useful :)
+ *@w: string to be measured
+ *@h: other string
+ * Return: Always 0 (Succeess)
+*/
+
+int **alloc_grid(int w, int h)
+{
+	int i, s;
+	int **dest;
+
+	if (w <= 0 || h <= 0)
+		dest = NULL;
+	else
+	{
+		dest = malloc(sizeof(int[h][w]));
+		if ( dest != NULL)
+		{
+			for (i = 0; i < h; i++)
+			{	
+				for (s = 0; i < w; s++)
+					dest[i][s] = 0;
+			}
+		}
+	}
+
+	return (dest);
+
+}
+
+/**
+ * _strlen - returns len of a string
+ * @s: string to be measured
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
