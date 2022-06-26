a981127d46446ea04f62bad6774e303de7da9451
Gonza <gonzabenitez070@gmail.com>
2022-06-26 10:38:48 -0700

    Add array range

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+# include "main.h"
+# include <stdlib.h>
+/**
+ * array_range - Something useful :)
+ *@min: string to be measured
+ *@max: size
+ * Return: Always 0 (Succeess)
+*/
+
+int *array_range(int min, int max)
+{
+	int *fg = NULL;
+	int i, diff;
+
+	if (min < max)
+	{
+		diff = max - min;
+		fg = malloc(diff);
+		if (fg != NULL)
+		{
+			diff = 0;
+			for (i = min; i <= max; i++)
+			{
+				fg[diff] = i;
+				diff++;
+			}
+		}
+	}
+	return (fg);
+}
+
