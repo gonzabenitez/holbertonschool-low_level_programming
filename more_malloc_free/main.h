86c39fac9c9de63789d47b0b50c265d12beb078d
Gonza <gonzabenitez070@gmail.com>
2022-06-26 10:40:10 -0700

    add main

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -11,4 +11,5 @@ void free_grid(int **, int);
 void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *,unsigned int);
 void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
 # endif
