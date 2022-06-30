c3e326eafc81c34e45f050e01bc2d3acfec54a15
Gonza <gonzabenitez070@gmail.com>
2022-06-30 10:02:38 -0700

    add dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,16 +11,18 @@ void print_dog(struct dog *d)
 	char **n, **o;
 	char *nil = "(nil)";
 
-	if ((*d).name != NULL)
-		n = &(*d).name;
-	else
-		n = &nil;
-	if ((*d).owner != NULL)
-		o = &(*d).owner;
-	else
-		o = &nil; 
 	if (d != NULL)
+	{
+		if ((*d).name != NULL)
+			n = &(*d).name;
+		else
+			n = &nil;
+		if ((*d).owner != NULL)
+			o = &(*d).owner;
+		else
+			o = &nil; 
 		printf("Name: %s\nAge: %f\nOwner: %s\n", *n, (*d).age, *o);
+	}
 	else
 		printf("\n");
 }
