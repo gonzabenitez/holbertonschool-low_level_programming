0ba6ac2d3167a9666da465ee8b5cd36fe30806f4
Gonza <gonzabenitez070@gmail.com>
2022-06-30 13:04:22 -0700

    Add all

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
 #ifndef fpoint
 #define fpoint
+# include <stdio.h>
+# include <stdlib.h>
+# include <stddef.h>
 void print_name(char *, void(*f)(char *));
 void array_iterator(int *arr, size_t s, void (*action)(int))
 #endif
