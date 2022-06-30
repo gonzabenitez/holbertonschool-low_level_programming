6b5e22291e65ab61370d9df52d83c7ad788c4c22
Gonza <gonzabenitez070@gmail.com>
2022-06-30 09:39:02 -0700

    add dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+# include "dog.h"
+# include <stdio.h>
+/**
+ * print_dog - Something useful :)
+ *@d: pointer to an in
+ * Return: Always 0 (Succeess)
+*/
+
+void init_dog(struct dog *d)
+{
+	if (d == 0)
+		return;
+	else
+		printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+}
