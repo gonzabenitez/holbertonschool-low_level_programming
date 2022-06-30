a40d9d58d93d83adb1f1ab16a0efdbbdc23aa1e9
Gonza <gonzabenitez070@gmail.com>
2022-06-30 15:58:25 -0700

    Add test

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -26,9 +26,9 @@ int (*get_op_func(char *s))(int a, int b)
 
 	while (i < 6)
 	{
-		if (s == (*ops[i]).op)
+		if (s == ops.op[i])
 			break;
 		i++;
 	}
-	return ((*ops[i]).f);
+	return (ops.f[i]);
 }
